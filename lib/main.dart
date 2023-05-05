9e9baf540db605b148253eebbcdb653ac71f348c
Ahmed Ezzt <124591295+ahmed3zzt@users.noreply.github.com>
2023-05-05 16:31:05 +0300

    Add Firebase Files

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:scholar_chat/pages/register_page.dart';
 
 import 'pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
